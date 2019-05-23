4cf3dc38279fbe7160a3641eb24d093fb596ac55
mvasalos <mvasalos@vertoyo.com>
2019-05-23 16:39:59 +0300

    notos-testing

--- src/main/java/com/example/demobcknd/controllers/UserController.java
+++ src/main/java/com/example/demobcknd/controllers/UserController.java
@@ -5,12 +5,14 @@ import com.example.demobcknd.services.userService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // This means that this class is a Controller
+@CrossOrigin  // allow crossorigin
 @RequestMapping(path = "/user") // This means URL's start with /demo (after Application path)
 public class userController {
 	@Autowired // This means to get the bean called userRepository
--- src/main/java/com/example/demobcknd/controllers/performanceController.java
+++ src/main/java/com/example/demobcknd/controllers/performanceController.java
@@ -5,12 +5,14 @@ import com.example.demobcknd.services.performanceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@CrossOrigin 
 @RequestMapping(path = "/performance")
 public class performanceController {
     @Autowired
